8fa97277c739f88e6464faa13607d7f30f256872
unknown <C14464428@mydit.ie>
2018-03-13 09:25:57 +0000

    Initial Scene setup

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    public float maximumSpeed = 10;
+    public float mass = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
